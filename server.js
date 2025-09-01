60a2f83dfb23efcdb4dbf9e9aa1c034fabf087b7
meow-20 <bansaripanchal2021@gmail.com>
2025-09-01 23:19:26 +0100

    Controllers, db, Middlewares, utils

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
+require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-dotenv.config();
+const userRoutes = require("./routes/userRoutes");
+const walletRoutes = require("./routes/walletRoutes");
+const bankRoutes = require("./routes/bankRoutes");
+const cardRoutes = require("./routes/cardRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+
 const app = express();
 
-app.use(cors());
+// middleware
 app.use(express.json());
 
-const authRoutes = require("./routes/authRoutes");
+// database
+connectDB();
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error(err));
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/wallets", walletRoutes);
+app.use("/api/banks", bankRoutes);
+app.use("/api/cards", cardRoutes);
+app.use("/api/transactions", transactionRoutes);
 
-  app.use("/", authRoutes);
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something broke!" });
+});
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
